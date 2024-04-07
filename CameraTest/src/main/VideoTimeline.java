9ddd57030bebea238522e0e9fd74d9164d7fd74f
mjjm4d <threehalves1@gmail.com>
2024-04-06 20:23:33 -0500

    add constructor and setter to VideoTimeline

--- CameraTest/src/main/VideoTimeline.java
+++ CameraTest/src/main/VideoTimeline.java
@@ -4,9 +4,24 @@ import java.util.ArrayList;
 
 public class VideoTimeline {
 	private ArrayList<Keyframe> keyframes;
+	
+	public VideoTimeline()
+	{
+		this.keyframes = new ArrayList<>();
+	}
+	
+	public VideoTimeline(ArrayList<Keyframe> k)
+	{
+		this.keyframes = k;
+	}
 
 	public ArrayList<Keyframe> getKeyframes() {
 		return keyframes;
+	}
+	
+	public void setKeyframes(ArrayList<Keyframe> k)
+	{
+		keyframes = k;
 	}
 	
 	
