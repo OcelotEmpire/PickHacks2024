65b91b6bb342fe3311094a13383f6cdc25e2b895
OcelotEmpire <hobbz.pi@gmail.com>
2024-04-07 09:15:37 -0500

    dancestorage

--- CameraTest/src/main/Mystical.java
+++ CameraTest/src/main/Mystical.java
@@ -11,11 +11,12 @@ import org.opencv.core.Core;
 public class Mystical {
 	private Thinker thinker;
 	private Observer observer;
-	// private Speaker speaker;
+	private Speaker speaker;
+	private Viewer viewer;
 	
 	private boolean inProgress = false;
 	
-	private float progress = 0;
+	private double progress = 0;
 	
 	private static double dur = 10d;
 	private static double sTime = 1d;
@@ -24,7 +25,14 @@ public class Mystical {
 	
 	public Mystical() {
 		thinker = new Thinker(4);
-		observer = new Observer(0);
+		observer = new Observer(1);
+		try {
+			speaker = new Speaker();
+		} catch (LineUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		viewer = new Viewer(observer);
 	}
 	
 	public void beginDance(double duration, double sampleTime) {
@@ -41,22 +49,33 @@ public class Mystical {
 				e.printStackTrace();
 			}
 		
-		DanceStorage.getKeyframes();
+		var allFrames = DanceStorage.getKeyframes();
 		
 		observer.begin();
-		Speaker.playAudio("res/audio/mother3.wav");
+		speaker.playAudio("res/audio/mother3.wav");
 		
 		long then = System.nanoTime();
 		long now;
-		double acc = 0;
+		long totalElapsedNano = 0;
+		long sampleTimeNano = (long)(sampleTime * 1e9);
+		long acc = 0;
 		progress = 0;
+		float lastFrameStamp = 0;
+		int lastFrameId = 0;
 		while (progress < duration) {
 			now = System.nanoTime();
-			double delta = (now - then) * 1e-9d;
+			long delta = (now - then);
+			totalElapsedNano += delta;
 			acc += delta;
-			progress += delta;
-			if (acc > sampleTime) {
-				acc -= sampleTime;
+			progress = totalElapsedNano * 1e-9d;
+			if (lastFrameId+1 < allFrames.size() && progress > lastFrameStamp) {
+				Keyframe currentFrame = allFrames.get(lastFrameId);
+				viewer.showKeyframe(currentFrame);
+				lastFrameId++;
+				lastFrameStamp = currentFrame.getTimestamp();
+			}
+			if (acc > sampleTimeNano) {
+				acc -= sampleTimeNano;
 				Photograph photo = observer.snapshot();
 				thinker.think(photo);
 			}
@@ -73,8 +92,8 @@ public class Mystical {
 				System.out.println(keyframe.getPoints()[0] + ", " + keyframe.getTimestamp());
 				if (!DanceStorage.setToWrite)
 				{
-					System.out.println("FRAME SCORE: " + Keyframe.compare(keyframe, DanceStorage.GetClosestKeyToTime(keyframe.getTimestamp())));
-					scoreSum +=  Keyframe.compare(keyframe, DanceStorage.GetClosestKeyToTime(keyframe.getTimestamp()));
+					System.out.println("FRAME SCORE: " + Keyframe.compare(keyframe, DanceStorage.getClosestKeyToTime(keyframe.getTimestamp())));
+					scoreSum +=  Keyframe.compare(keyframe, DanceStorage.getClosestKeyToTime(keyframe.getTimestamp()));
 				}
 				DanceStorage.add(keyframe);
 			} catch (InterruptedException | ExecutionException e) {
@@ -101,14 +120,13 @@ public class Mystical {
 		return inProgress;
 	}
 
-	public float getProgress() {
+	public double getProgress() {
 		return progress;
 	}
 
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		Mystical mystical = new Mystical();
-		Viewer viewer = new Viewer(mystical.getObserver());
 		DanceStorage.getKeyframes();
 		mystical.beginDance(41, 1);
 	}
